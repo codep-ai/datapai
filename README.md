642c0380c81e84369a153bf7010c4920d53837d3
Donny Zhao <simon861102@gmail.com>
2024-04-14 23:24:19 +1000

    new repo

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# datapai
-home page
+# How to start
+1. `npm install`
+2. `node index.js`
+
+# Deploy:
+1. install nvm: `https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html`
+`curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash`
+2. install node
+
+
+# Reference
+1. Setup Gmail SMTP https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1
---
+++ public/js/codepai.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+  $('#btnSubmit').bind('click', function(e) {
+    e.preventDefault();
+    var payload = {
+      suburb: $('#inputSuburb').val(),
+      name: $('#inputName').val(),
+      email: $('#inputEmail').val(),
+      phone: $('#inputPhone').val(),
+      message: $('#inputMessage').val()
+    };
+    var error = '2px solid red';
+    var b = true;
+    if (!payload.suburb) {
+      b = false;
+      $('#inputSuburb').css('border', error);
+    }
+    if (!payload.name) {
+      b = false;
+      $('#inputName').css('border', error);
+    }
+    if (!payload.email) {
+      b = false;
+      $('#inputEmail').css('border', error);
+    }
+    if (!payload.phone) {
+      b = false;
+      $('#inputPhone').css('border', error);
+    }
+    if (!payload.suburb) {
+      b = false;
+      $('#inputMessage').css('border', error);
+    }
+    if (!b) {
+      return;
+    }
+    $.ajax({
+      type: 'POST',
+      url: '/contact',
+      contentType: 'application/json',
+      data: JSON.stringify(payload),
+      success: function(e) {
+        $('#message-success').show();
+        $('#contact-form').hide();
+      },
+      error: function(e) {
+        console.log(e);
+      }
+    });
+  });
+
+
+  $('#btnRegister').bind('click', function(e) {
+    e.preventDefault();
+    var payload = {
+      name: $('#name').val(),
+      year: $('#year').val(),
+      email: $('#email').val(),
+      phone: $('#phone').val(),
+      suburb: $('#suburb').val(),
+      state: $('#state').val(),
+      class: $('#class').val(),
+      remark: $('#remark').val()
+    };
+    var error = '2px solid red';
+    var b = true;
+    if (!payload.suburb) {
+      b = false;
+      $('#suburb').css('border', error);
+    }
+    if (!payload.name) {
+      b = false;
+      $('#name').css('border', error);
+    }
+    if (!payload.email) {
+      b = false;
+      $('#email').css('border', error);
+    }
+    if (!payload.phone) {
+      b = false;
+      $('#phone').css('border', error);
+    }
+    if (!payload.state) {
+      b = false;
+      $('#state').css('border', error);
+    }
+    if (!payload.class) {
+      b = false;
+      $('#class').css('border', error);
+    }
+    if (!b) {
+      return;
+    }
+    $.ajax({
+      type: 'POST',
+      url: '/register',
+      contentType: 'application/json',
+      data: JSON.stringify(payload),
+      success: function(e) {
+        $('#message-success').show();
+        $('#register-form').hide();
+      },
+      error: function(e) {
+        console.log(e);
+      }
+    });
+  });
+
+});
